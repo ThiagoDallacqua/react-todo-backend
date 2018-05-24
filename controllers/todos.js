38d31af8bf3a04d0a587625868708995fe68d028
Thiago Dallacqua <thiagodallacqua@outlook.com>
2018-05-23 23:47:55 -0300

    added controllers without auth middleware

--- controllers/todos.js
+++ controllers/todos.js
@@ -112,4 +112,84 @@ module.exports = app => {
       return
     });
   });
+
+  app.post('/todos', (req, res) => {
+    const model = app.models.todos;
+    const connect = app.infra.connectionFactory;
+    const Todos = new app.infra.TodoDAO(model, connect);
+
+    const todo = {
+      user: {
+        id: req.user._id,
+        username: req.user.username
+      },
+      todo: req.body.todo
+    };
+
+    Todos.create(todo, (err, response) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send(err)
+
+        return
+      }
+
+      res.json(response)
+      return
+    })
+  });
+
+  app.get('/todos', (req, res) => {
+    const model = app.models.todos;
+    const connect = app.infra.connectionFactory;
+    const Todos = new app.infra.TodoDAO(model, connect);
+
+    Todos.list(req.user._id, (err, result) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send(err)
+
+        return
+      }
+
+      const todo = result.map(element => {
+        const info = {
+          id: element.id,
+          todo: element.todo
+        }
+
+        return info
+      });
+
+      res.json(todo)
+    });
+  });
+
+  app.delete('/todos/:id', (req, res) => {
+    const model = app.models.todos;
+    const connect = app.infra.connectionFactory;
+    const Todos = new app.infra.TodoDAO(model, connect);
+    const id = req.params.id;
+
+    const args = {
+      userId: req.user._id,
+      todoId: id
+    }
+
+    Todos.delete(args, (err, result) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send(err)
+
+        return
+      }
+
+      let oprResult = {
+        todoResult: result
+      }
+
+      res.json(oprResult)
+      return
+    });
+  });
 }
